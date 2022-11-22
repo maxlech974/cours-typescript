c32fc9e15c76bbe6d3be4372a6ca3989b88e2936
Maxime LECHERE <maximelechere.dev@gmail.com>
2022-11-22 15:50:55 +0100

    classes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,67 @@
-// let add: (nbr1: number, nbr2: number) => number;
-let add = (...numbers: Array<number>) => {
-  numbers.reduce((acc, nbr) => {
-    acc += nbr;
-    return acc;
-  }, 0);
+abstract class Engine {
+  constructor(private type: string) {
+
+  }
+ 
+  abstract stopEngine(): void;
 }
 
-console.log(add(1, 2, 3, 4));
+class Vehicule extends Engine {
+  wheel= 4;
+  protected brand: string;
+
+  constructor(brand: string) {
+    super('V8');
+    this.brand = brand;
+  }
+}
+
+
+class Voiture extends Vehicule {
+  static className = "Véhicule";
+  private maxSpeed: number = 100;
+  readonly airbag: boolean = true;
+
+  static startCar() {
+    console.log('car start')
+  }
+
+  faster(newSpeed: number): void {
+    if(newSpeed <= this.maxSpeed) {
+      this.speed = newSpeed;
+    }
+  }
+  move() {
+    console.log('car move')
+  }
+
+  stop() {
+    console.log('car stop')
+  }
+
+  changeBrand(newBrand: string): void {
+    this.brand = newBrand;
+  }
+
+
+  constructor (public speed: number, brand: string) {
+    super(brand);
+    this.speed = speed;
+  }
+
+  stopEngine(): void {
+    console.log('stop engine')
+  }
+}
+
+let car: Voiture;
+
+car = new Voiture(300, 'BMW');
+// car.changeBrand('Peugeot');
+console.log(car);
+console.log(Voiture.className);
+console.log(Voiture.startCar());
+
+
+
+console.log(car.wheel);
